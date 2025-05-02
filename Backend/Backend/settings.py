e6a89cb74a14a5c2e275c859924adaada44cf2e4
Yahor Vashkevich <eg-vashkevich@eg-vashkevich.vla.yp-c.yandex.net>
2025-05-02 21:04:12 +0300

    back: remove comment from settings.py

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -233,30 +233,11 @@ AUTH_USER_MODEL = "users.User"
 
 ################################################################################
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     ),
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-#         'rest_framework.authentication.SessionAuthentication',
-#         'rest_framework.authentication.BasicAuthentication',
-#     ),
-#     'NON_FIELD_ERRORS_KEY': 'global',
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
-#         'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
-#     ),
-#     'DEFAULT_PARSER_CLASSES': (
-#         'djangorestframework_camel_case.parser.CamelCaseFormParser',
-#         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
-#         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
-#     ),
-# }
-
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "custom_auth.authentication.CookieJWTAuthentication",
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
